319395df07ed7ad45b50c8981af28bc2bc40a3d9
Kyna Moroney <bkmoroney@outlook.com>
2025-02-27 15:41:44 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
+//DOM load
+function loaded() {}
+
 /*Glide.js Object Settings*/
-  const config = {
+const config = {
     type: 'slider',
     bound: true,
     gap: 10,
     perView: 7,
-    breakpoints:{
-      1920: {
-        perView: 5
-      },
-      1024: {
-        perView: 3
-      },
-      650: {
-        perView: 1
-      }
+    breakpoints: {
+        1920: {
+            perView: 5
+        },
+        1024: {
+            perView: 3
+        },
+        650: {
+            perView: 1
+        }
     }
 }
 
@@ -23,16 +26,13 @@ new Glide('.glide', config).mount()
 const lightbox = GLightbox({
     touchNavigation: true,
     loop: true,
-  });
+});
 
 //Mobile Nav Toggle
-function showNav(){
-  var x = document.getElementById("topNav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-    document.body.style.padding = "0";
-  } else {
-    x.className = "topnav";
-    document.body.style.padding = "20px";
-  }
+function openNav() {
+    document.getElementById("topNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("topNav").style.height = "0%";
 }
